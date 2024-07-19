bd830950d836bea77f4230106b55929d795dd6d6
Lukas Baier <lukasbaier@outlook.de>
2024-07-19 17:48:40 +0200

    renamed vars

--- main.c
+++ main.c
@@ -24,7 +24,7 @@
 #define SIZE 10
 
 int main() {
-	char *testSuite[SIZE] = {
+	char *test_suite[SIZE] = {
 			"(a|b|c|d|e|f)",
 			"(7*e*|d*)l",
 			"I+",
@@ -37,7 +37,7 @@ int main() {
             "d[a-f]t"
 	};
 
-	char *testSuiteMatch[SIZE] = {
+	char *test_suite_match[SIZE] = {
 			"b",
 			"3",
 			"I'm down bad ngl",
@@ -54,25 +54,25 @@ int main() {
 	double time_spent;
 
 	regex_node *tree;
-	seek *tokenStream;
+	seek *tokenstream;
 
 	dfa *dfa;
 
 	int i;
 	for (i = 0; i < SIZE; i++) {
 		begin = clock();
-		tokenStream = lexer_tokenize(testSuite[i], (char) strnlen(testSuite[i], 256));
+        tokenstream = lexer_tokenize(test_suite[i], (char) strnlen(test_suite[i], 256));
 		end = clock();
 		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-		printf("Tokenizing \"%s\": \x1b[32m%f\x1b[0m\n", testSuite[i], time_spent);
+		printf("Tokenizing \"%s\": \x1b[32m%f\x1b[0m\n", test_suite[i], time_spent);
 
 		begin = clock();
-		tree = parser_parse(tokenStream, PR_LOWEST);
+		tree = parser_parse(tokenstream, PR_LOWEST);
 		end = clock();
 		time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-		printf("parsing \"%s\": \x1b[32m%f\x1b[0m\n", testSuite[i], time_spent);
+		printf("parsing \"%s\": \x1b[32m%f\x1b[0m\n", test_suite[i], time_spent);
 
         regex_print_regexp(tree);
         putchar('\n');
@@ -82,14 +82,14 @@ int main() {
         end = clock();
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-        printf("pattern 2 dfa \"%s\": \x1b[32m%f\x1b[0m\n", testSuite[i], time_spent);
+        printf("pattern 2 dfa \"%s\": \x1b[32m%f\x1b[0m\n", test_suite[i], time_spent);
 
         begin = clock();
-        _Bool matched = match_dfa(dfa, testSuiteMatch[i], (int) strnlen(testSuiteMatch[i], 256));
+        _Bool matched = match_dfa(dfa, test_suite_match[i], (int) strnlen(test_suite_match[i], 256));
         end = clock();
         time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-        printf("regex_match \"%s\" with \"%s\" preemptively: \x1b[32m%f\x1b[0m\n", testSuite[i], testSuiteMatch[i], time_spent);
+        printf("regex_match \"%s\" with \"%s\" preemptively: \x1b[32m%f\x1b[0m\n", test_suite[i], test_suite_match[i], time_spent);
 
 		puts("\x1b[31m------\x1b[0m");
 	}
